7b755f81dd03bd3e521fc51f0377b11d98ea9df9
Eric Anderson <ehander3@ncsu.edu>
2015-12-04 15:45:01 -0500

    Measure

--- project/static/js/measure.js
+++ project/static/js/measure.js
@@ -1,4 +1,6 @@
 var count = 0;
+var maxCount = 1;
+var measurements = [];
 
 $(function() {
 		$(".tooth").draggable();
@@ -11,5 +13,17 @@ $(function() {
 		});
 		$("img").hide();
 		$("#tooth" + count).show();
+		
+		$("#measureButton").click(function(){
+			measurements.push($("#selectSize").val());
+			console.log(measurements);
+			$("#tooth" + count).hide();
+			count++;
+			if (count > maxCount){
+				//go to stats page
+			} else {
+				$("#tooth" + count).show();
+			}
+		});
 	});
 	
