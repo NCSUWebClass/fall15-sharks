827ad0b8f7a89d9d1dd634980726c3adc1db5515
Eric Anderson <ehander3@ncsu.edu>
2015-12-04 15:33:45 -0500

    Measure.

--- project/static/js/measure.js
+++ project/static/js/measure.js
@@ -3,9 +3,8 @@ $(function() {
 		$(".measure").droppable({
 			drop: function(event, ui) {
 				$(this)
-				.addClass("ui-state-highlight")
-				.find("p")
-					.html("Your tooth is X pixels");
+				.addClass("ui-state-highlight");
+				
 			}
 		});
 	});
