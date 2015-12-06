8393711a1bf815849b53e6a5c1373980b7f401a0
wepruett <wepruett@ncsu.edu>
2015-12-06 12:57:40 -0500

    fixed missing function

--- project/static/js/virtualdig.js
+++ project/static/js/virtualdig.js
@@ -203,3 +203,14 @@
         teethData = JSON.parse(teethStr);
 	}
 	
+	//function found at http://www.w3schools.com/js/js_cookies.asp
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        }
+        return "";
+    }
