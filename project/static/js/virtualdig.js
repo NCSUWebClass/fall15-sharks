a4ba3afd4351f8b34346bc19db5cc8cd60bd6d41
DerekSchreiner <dsschrei@ncsu.edu>
2015-12-08 14:59:57 -0500

    trying to fix hidden teeth

--- project/static/js/virtualdig.js
+++ project/static/js/virtualdig.js
@@ -176,7 +176,7 @@
 
 	function removeTeeth(sectionIndex) {
 		for (var i = 0; i < teeth[sectionIndex].length; i++) {
-			console.log($('#' + teeth[sectionIndex][i].id).parent()[0].id);
+			console.log($('#' + teeth[sectionIndex][i].id).parent()[0].id === 'game-container');
 			if(teeth[sectionIndex][i].parent) {
 				teeth[sectionIndex][i].style.display = 'none';
 			}
