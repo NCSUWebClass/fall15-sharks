7eb94dd641c997b7ebdce15eb7defe40143183ac
DerekSchreiner <dsschrei@ncsu.edu>
2015-12-08 14:54:33 -0500

    trying to fix hidden teeth

--- project/static/js/virtualdig.js
+++ project/static/js/virtualdig.js
@@ -176,7 +176,7 @@
 
 	function removeTeeth(sectionIndex) {
 		for (var i = 0; i < teeth[sectionIndex].length; i++) {
-			console.log(document.getElementById([sectionIndex][i].id).parent());
+			console.log(document.getElementById(teeth[sectionIndex][i].id).parent());
 			if(teeth[sectionIndex][i].parent) {
 				teeth[sectionIndex][i].style.display = 'none';
 			}
